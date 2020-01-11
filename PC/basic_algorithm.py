1ffeb8d1ac243ac67504e274a39b8d8889a8ce20
wonjaegang <robinsam@naver.com>
2020-01-12 04:30:53 +0900

    add algorithm converts inputs to calls / change basement floor from -1 to 0 / modify output format

--- PC/basic_algorithm.py
+++ PC/basic_algorithm.py
@@ -4,95 +4,67 @@ from time import sleep
 
 
 # Class indicates specification of the building. Use decimal module to avoid floating point error
+# 0th floor is a basement floor
 class Building:
         floor_height = decimal.Decimal('2.5')
-        lowest_f = -1
+        lowest_f = 0
         highest_f = 5
-        lowest_m = floor_height * lowest_f
-        highest_m = floor_height * highest_f
-        whole_floor = abs(lowest_f) + highest_f
+        lowest_m = floor_height * (lowest_f - 1)
+        highest_m = floor_height * (highest_f - 1)
+        whole_floor = highest_f - lowest_f + 1
 
 
 class Elevator:
     speed = decimal.Decimal('0.1')  # 0.1m/loop
 
-    def __init__(self, num, location):  # initialize instance
-        self.num = num
+    def __init__(self, id_num, location):  # initialize instance
+        self.id_num = id_num
         self.location = location
 
     def commend(self, motion):
         if motion == 'u':
             if self.location == Building.highest_m:
-                raise IndexError("Elevator%d is on the highest floor" % self.num)
+                raise IndexError("Elevator%d is on the highest floor" % self.id_num)
             self.location += Elevator.speed
         elif motion == 'd':
             if self.location == Building.lowest_m:
-                raise IndexError("Elevator%d is on the lowest floor" % self.num)
+                raise IndexError("Elevator%d is on the lowest floor" % self.id_num)
             self.location -= Elevator.speed
 
     def __str__(self):
-        return "Elevator{x} location : {y}m".format(x=self.num, y=self.location)
+        return "Elevator{x} location : {y}m".format(x=self.id_num, y=self.location)
 
 
 # Global variables
-lc = [[False] * (Building.whole_floor + 1) for i in range(2)]
+# cc : Car Call      [floor(-1) <- [down, up], floor(1) <- [down, up], ... floor(5) <- [down, up]]
+# lc : Landing Call  [ Ele(1) <- [0, 1, 2, 3, 4, 5, open], Ele(2) <- [0, 1, 2, 3, 4, 5, open]]
 cc = [[False] * 2 for k in range(Building.whole_floor)]
+lc = [[False] * (Building.whole_floor + 1) for i in range(2)]
+cc_button_num = len(cc) * 2 - 2  # Except lowest down, highest up
 
 
-# Function that converts button inputs to the Landing Calls and the Car Calls
+# Function that converts button inputs to the Car Calls and the Landing Calls
 # It modifies global variables
 def input_to_call():
     check = False
-    ardu = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)    # revise port's name for each PC after
+    ardu = serial.Serial(port='COM6', baudrate=9600)    # revise port's name for each PC after
     if ardu.readable():     # If there is a button input
         data = ardu.readline()
-        # using data variable, changes global input
-        if data == b'A\r\n':
-            # turns to True 
-        elif data == b'B\r\n':
-            # turns to True
-        elif data == b'C\r\n':
-            # turns to True
-        elif data == b'D\r\n':
-            # turns to True
-        elif data == b'E\r\n':
-            # turns to True
-        elif data == b'F\r\n':
-            # turns to True
-        elif data == b'G\r\n':
-            # turns to True
-        elif data == b'H\r\n':
-            # turns to True
-        elif data == b'I\r\n':
-            # turns to True
-        elif data == b'J\r\n':
-            # turns to True
-        elif data == b'K\r\n':
-            # turns to True
-        elif data == b'L\r\n':
-            # turns to True
-        elif data == b'M\r\n':
-            # turns to True
-        elif data == b'N\r\n':
-            # turns to True
-        elif data == b'O\r\n':
-            # turns to True
-        elif data == b'Q\r\n':
-            # turns to True
-        elif data == b'R\r\n':
-            # turns to True
-        elif data == b'S\r\n':
-            # turns to True
-        elif data == b'T\r\n':
-            # turns to True
-        elif data == b'U\r\n':
-            # turns to True
-        elif data == b'V\r\n':
-            # turns to True
-        elif data == b'W\r\n':
-            # toggle True and False
-        elif data == b'X\r\n':
-            # toggle True and False
+        int_data = int.from_bytes(data, "little") - int.from_bytes(b'A\r\n', "little")  # Convert to int starts from 0
+        if int_data < cc_button_num:
+            cc_floor = (int_data + 1) // 2
+            cc_direction = (int_data + 1) % 2
+            cc[cc_floor][cc_direction] = True
+        elif int_data < cc_button_num + Building.whole_floor * 2:
+            lc_id = (int_data - cc_button_num) // Building.whole_floor
+            lc_floor = (int_data - cc_button_num) % Building.whole_floor
+            lc[lc_id][lc_floor] = True
+        elif int_data < cc_button_num + Building.whole_floor * 2 + 2:
+            open_id = int_data - (cc_button_num + Building.whole_floor * 2)
+            lc[open_id][Building.whole_floor] = True
+        else:
+            raise ValueError("Serial input is NOT proper")
+        print("Button Board says (", data, ") which means", int_data, "th button")
         check = True
     # if elevator completes a work :
         # using whatever such as location or other variable
@@ -101,13 +73,13 @@ def input_to_call():
     return check
 
 
-# Main algorithm that converts the Landing Calls and the Car Calls to the motion of each elevator
+# Main algorithm that converts the Car Calls and the Landing Calls to the motion of each elevator
 # It uses global variables as arguments
 def call_to_commend():
 
     # Need Algorithm
 
-    motion = ['u', 'd']  # example
+    motion = ['d', 'u']  # example
     return motion
 
 
@@ -122,8 +94,9 @@ while True:
     elevator1.commend(commend[0])
     elevator2.commend(commend[1])
     # Print with certain format -> sent to GUI algorithm
+    print("Car call : ", cc)
+    print("Elevator1 Landing call : ", lc[0])
+    print("Elevator2 Landing call : ", lc[1])
     print(elevator1)
     print(elevator2)
-    print(lc)
-    print(cc)
     sleep(0.1)
