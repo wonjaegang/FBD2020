adb33293e78834a1bd394b999f934de51e61174c
Kim, Kyeong-Min <ts.kmkim@gmail.com>
2020-01-16 17:35:05 +0900

    code-format:: PEP8

--- PC/kmkim_test/animation_pygame.py
+++ PC/kmkim_test/animation_pygame.py
@@ -14,30 +14,30 @@ pygame.init()
 pygame.display.set_caption("Pygame Test")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-font = pygame.font.Font('freesansbold.ttf',30)
-text_B1 = font.render("B1",True,white)
-text_1 = font.render("1",True,white)
-text_2 = font.render("2",True,white)
-text_3 = font.render("3",True,white)
-text_4 = font.render("4",True,white)
-text_5 = font.render("5",True,white)
-
+font = pygame.font.Font('freesansbold.ttf', 30)
+text_B1 = font.render("B1", True, white)
+text_1 = font.render("1", True, white)
+text_2 = font.render("2", True, white)
+text_3 = font.render("3", True, white)
+text_4 = font.render("4", True, white)
+text_5 = font.render("5", True, white)
 
 
 def print_building():
-	screen.fill(black)
-	pygame.draw.line(screen, white, [640, SIZE], [0, SIZE], 3)
-	pygame.draw.line(screen, white, [640, 2*SIZE], [0, 2*SIZE], 3)
-	pygame.draw.line(screen, white, [640, 3*SIZE], [0, 3*SIZE], 3)
-	pygame.draw.line(screen, white, [640, 4*SIZE], [0, 4*SIZE], 3)
-	pygame.draw.line(screen, white, [640, 5*SIZE], [0, 5*SIZE], 3)
-	pygame.draw.line(screen, white, [640, 6*SIZE], [0, 6*SIZE], 3)
-	screen.blit(text_B1,(500, 6*SIZE-30))
-	screen.blit(text_1,(500, 5*SIZE-30))
-	screen.blit(text_2,(500, 4*SIZE-30))
-	screen.blit(text_3,(500, 3*SIZE-30))
-	screen.blit(text_4,(500, 2*SIZE-30))
-	screen.blit(text_5,(500, SIZE-30))	
+    screen.fill(black)
+    pygame.draw.line(screen, white, [640, SIZE], [0, SIZE], 3)
+    pygame.draw.line(screen, white, [640, 2*SIZE], [0, 2*SIZE], 3)
+    pygame.draw.line(screen, white, [640, 3*SIZE], [0, 3*SIZE], 3)
+    pygame.draw.line(screen, white, [640, 4*SIZE], [0, 4*SIZE], 3)
+    pygame.draw.line(screen, white, [640, 5*SIZE], [0, 5*SIZE], 3)
+    pygame.draw.line(screen, white, [640, 6*SIZE], [0, 6*SIZE], 3)
+    screen.blit(text_B1, (500, 6*SIZE-30))
+    screen.blit(text_1, (500, 5*SIZE-30))
+    screen.blit(text_2, (500, 4*SIZE-30))
+    screen.blit(text_3, (500, 3*SIZE-30))
+    screen.blit(text_4, (500, 2*SIZE-30))
+    screen.blit(text_5, (500, SIZE-30))
+
 
 #clock = pygame.time.Clock()
 pos_x = 200
@@ -46,27 +46,27 @@ print_building()
 pygame.draw.rect(screen, red, [pos_x, pos_y, 25, SIZE], 5)
 pygame.display.update()
 while True:
-	y=(input("B1, 1, 2, 3, 4, 5 중 택 1: "))
-	if y == 'B1':
-		y=500
-	else:
-		y=int(y)
-		y+=1
-		y=600-y*SIZE	
-	print_building()
-	pygame.display.update()
-	if y == pos_y:
-		print_building()
-		pygame.draw.rect(screen, red, [pos_x, pos_y, 25, SIZE], 5)
-		pygame.display.update()
-	else:
-		while y != pos_y:
-			#clock.tick(10)
-			if y <= pos_y:
-				pos_y -= 4
-			elif y >= pos_y:
-				pos_y += 4
-			print_building()
-			pygame.draw.rect(screen, red, [pos_x, pos_y, 25, SIZE], 5)
-			pygame.display.update()
-			time.sleep(0.1)
+    y = (input("B1, 1, 2, 3, 4, 5 중 택 1: "))
+    if y == 'B1':
+        y = 500
+    else:
+        y = int(y)
+        y += 1
+        y = 600-y*SIZE
+    print_building()
+    pygame.display.update()
+    if y == pos_y:
+        print_building()
+        pygame.draw.rect(screen, red, [pos_x, pos_y, 25, SIZE], 5)
+        pygame.display.update()
+    else:
+        while y != pos_y:
+            # clock.tick(10)
+            if y <= pos_y:
+                pos_y -= 4
+            elif y >= pos_y:
+                pos_y += 4
+            print_building()
+            pygame.draw.rect(screen, red, [pos_x, pos_y, 25, SIZE], 5)
+            pygame.display.update()
+            time.sleep(0.1)
--- PC/kmkim_test/serial_input.py
+++ PC/kmkim_test/serial_input.py
@@ -3,12 +3,13 @@
 # 시리얼 입력 받은 데이터를 출력하는 코드
 import serial
 from time import sleep
-num=1
-ardu = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)    # revise port's name for each PC after
-    
+num = 1
+# revise port's name for each PC after
+ardu = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)
+
 # ardu = serial.Serial(port='/dev/ttyUSB0',baudrate=9600)
 # port의 ttyUSB0은 연결된 시리얼 포트의 이름으로 수정할 것
-# baudrate는 9600 고정 
+# baudrate는 9600 고정
 
 
 def input_to_call():
@@ -17,19 +18,20 @@ def input_to_call():
     print(data)
     if data == b'A\r\n':
         print('A')
-        check=True
+        check = True
     elif data == b'B\r\n':
         print('B')
-        check=True
+        check = True
     elif data == b'W\r\n':
         print('W')
-        check=True        
+        check = True
     return check
+
 
 while True:
     print(num)
-    num=num+1
-    call_change=input_to_call()
+    num = num+1
+    call_change = input_to_call()
     if call_change:
-       print("hi")
+        print("hi")
     sleep(0.1)
