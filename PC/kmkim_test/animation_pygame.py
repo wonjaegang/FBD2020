68c51461cb12cec0bb77c0219a415b108374a453
Kim, Kyeong-Min <ts.kmkim@gmail.com>
2020-01-16 11:06:15 +0900

    GUI example code

---
+++ PC/kmkim_test/animation_pygame.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+SIZE = 20
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+pygame.init()
+pygame.display.set_caption("Pygame Test")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+clock = pygame.time.Clock()
+pos_x = 200
+pos_y = 480
+while True:
+	y=int(input("입력: "))
+	screen.fill(black)
+	pygame.display.update()
+	while y != pos_y:
+		clock.tick(10)
+		if y < pos_y:
+			pos_y -= 5
+		elif y > pos_y:
+			pos_y += 5
+		screen.fill(black)
+		pygame.draw.rect(screen, white, [pos_x, pos_y, 2 * SIZE, 4 * SIZE], 10)
+		pygame.display.update()
