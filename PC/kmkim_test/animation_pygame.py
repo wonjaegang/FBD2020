1748cdd4b8eba96e1044a241dfc127bb3bb38356
Kim, Kyeong-Min <ts.kmkim@gmail.com>
2020-01-16 11:27:19 +0900

    rm pygame_clock.tick and add time.sleep

--- PC/kmkim_test/animation_pygame.py
+++ PC/kmkim_test/animation_pygame.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import time
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
@@ -12,7 +13,7 @@ pygame.init()
 pygame.display.set_caption("Pygame Test")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-clock = pygame.time.Clock()
+#clock = pygame.time.Clock()
 pos_x = 200
 pos_y = 480
 while True:
@@ -20,7 +21,7 @@ while True:
 	screen.fill(black)
 	pygame.display.update()
 	while y != pos_y:
-		clock.tick(10)
+		#clock.tick(10)
 		if y < pos_y:
 			pos_y -= 5
 		elif y > pos_y:
@@ -28,3 +29,4 @@ while True:
 		screen.fill(black)
 		pygame.draw.rect(screen, white, [pos_x, pos_y, 2 * SIZE, 4 * SIZE], 10)
 		pygame.display.update()
+		time.sleep(0.1)
